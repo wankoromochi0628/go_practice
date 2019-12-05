f0dc61371f417e9168b379378f8fae6fc809b7bf
wankoromochi0628 <wankoromoti0628@gmail.com>
2019-12-05 23:38:44 +0900

    [add] test code

--- lesson5/main.go
+++ lesson5/main.go
@@ -7,5 +7,11 @@ import (
 
 func main() {
 	s := []int {1, 2, 3, 4, 5}
+
+	// UpperCaseのpublic変数にはアクセス可
+	fmt.Println(mylib.Public)
+	// LowerCaseのprivate変数にはアクセス不可
+	//fmt.Println(mylib.private)
+
 	fmt.Println(mylib.Average(s))
 }
--- lesson5/mylib/math.go
+++ lesson5/mylib/math.go
@@ -1,11 +1,21 @@
 package mylib
 
+import "golang.org/x/tools/go/ssa/interp/testdata/src/fmt"
+
+// UpperCase : public
+var Public string = "Public"
+// LowerCase : private
+var private string = "private"
+
 func Average(s []int)int {
 	total := 0
 
 	for _, i := range s {
 		total += i
 	}
+
+	fmt.Println(Public)
+	fmt.Println(private)
 
 	return int(total/len(s))
 }
